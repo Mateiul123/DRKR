e0bdb9450aa84d04ded50c572d624804ae283766
Matei Pop <popmatei70@gmail.com>
2024-06-03 07:05:32 +0100

    Final

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DRKR.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+}
